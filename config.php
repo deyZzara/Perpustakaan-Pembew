6df34b19614666bf9aa209d4cab54b0854aec9c0
fadey <167962210+deyZzara@users.noreply.github.com>
2025-06-03 14:56:34 +0700

    Update config.php
    for deploying

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
-$host = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'uasandroids4';
-
+$host = getenv("MYSQL_ADDON_HOST");
+$username = getenv("MYSQL_ADDON_USER");
+$password = getenv("MYSQL_ADDON_PASSWORD");
+$database = getenv("MYSQL_ADDON_DB");
+$port = getenv("MYSQL_ADDON_PORT");
+//
 $conn = mysqli_connect($host, $username, $password, $database);
 
 if (!$conn) {
